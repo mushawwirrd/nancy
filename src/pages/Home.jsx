269aaefcd50d79873176771c204247627bafe9fd
mushawwir <mushawwirrd7@gmail.com>
2025-11-12 10:27:41 +0700

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,7 +13,7 @@ function Home() {
     return (
         <div className="flex items-center justify-center mt-6">
             <div className="blok text-center ">
-                <h3 className="text-4xl font-semibold py-3 mb-2">Mari kita buat projectnya</h3>
+                <h3 className="text-4xl font-semibold py-3 mb-2">Hitung</h3>
                 <button onClick={BuatProject} className="bg-sky-500 hover:shadow-lg focus:ring ring-sky-400 rounded-full px-6 py-3 text-lg font-medium text-white" >Buat Project</button>
             </div>
         </div>
